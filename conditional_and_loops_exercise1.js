56ffba3b796f68aaa8345624a8f887b117dc24d7
Arshya <codingbyarshya@gmail.com>
2023-03-14 14:22:38 +0300

    conditional_and_loops_exercise1

---
+++ conditional_and_loops_exercise1.js
@@ -0,0 +1,35 @@
+function calculateSalary(role) {
+    let salary = 0;
+    switch (role) {
+      case "ceo":
+        salary = 2200;
+        break;
+      case "manager":
+        salary = 1800;
+        break;
+      case "cto":
+        salary = 1800;
+        break;
+      case "developer":
+        salary = 1500;
+        break;
+      default:
+        salary = 1000;
+        break;
+    }
+    return salary + "€";
+  }
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
+
+  
